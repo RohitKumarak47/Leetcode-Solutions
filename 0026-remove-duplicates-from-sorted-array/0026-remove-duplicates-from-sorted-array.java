d7e2b40aa6fdc19986ea784045b8469a078745f1
Rohit Kumar <kumarrohit23838@gmail.com>
2026-07-08 12:35:31 +0530

    Update 0026-remove-duplicates-from-sorted-array.java

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,8 +4,8 @@ class Solution {
        int j;
        for(j=1;j<nums.length;j++){
         if(nums[i]!=nums[j]){
-        nums[i]=nums[j];
-             i++;  
+              i++; 
+        nums[i]=nums[j]; 
         }
        }
      return i+1;
